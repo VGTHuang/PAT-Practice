2b3e23f9a900a35b20d4a1dc8e87e58b710e31d8
VGTHuang <36185631+VGTHuang@users.noreply.github.com>
2018-02-20 11:36:10 +0800

    Update 1013.cpp

--- 1013.cpp
+++ 1013.cpp
@@ -3,8 +3,7 @@
 /*
 题目描述
 The task of this problem is simple: insert a sequence of distinct positive integers 
-into a hash table, and output the positions of the input numbers.  The hash function 
-is defined to be "H(key) = key % TSize" where TSize is the maximum size of the hash 
+into a hash table, and output the positions of the input numbers.  The hash function is defined to be "H(key) = key % TSize" where TSize is the maximum size of the hash 
 table.  Quadratic probing (with positive increments only) is used to solve the collisions.
 Note that the table size is better to be prime.  If the maximum size given by the user 
 is not prime, you must re-define the table size to be the smallest prime number which 
@@ -72,7 +71,8 @@ int main() {
 	int* table = (int*)malloc(sizeof(int)*tablesize);
 	for (i = 0; i < tablesize; i++) table[i] = 0;  //inputs are positive integers. 0 represents empty
 	for (i = 0; i < inputnum; i++) {
-		cin >> tempinput;Hashpush(tempinput, table, tablesize);
+		cin >> tempinput;
+		Hashpush(tempinput, table, tablesize);
 		if (i < inputnum - 1) cout << ' ';
 	}
 	free(table);
