a8b870d4683eca45f924713260b5546a10bf4a09
VGTHuang <36185631+VGTHuang@users.noreply.github.com>
2018-02-20 19:16:00 +0800

    Update 1014.cpp

--- 1014.cpp
+++ 1014.cpp
@@ -1,24 +1,24 @@
 //PAT 1014, 2018/2/20
 
 /*
-ÌâÄ¿ÃèÊö
-A supply chain is a network of retailers£¨ÁãÊÛÉÌ£©, distributors£¨¾­ÏúÉÌ£©, and suppliers£¨¹©Ó¦ÉÌ£©-- everyone involved in moving a product from supplier to customer.
+é¢˜ç›®æè¿°
+A supply chain is a network of retailersï¼ˆé›¶å”®å•†ï¼‰, distributorsï¼ˆç»é”€å•†ï¼‰, and suppliersï¼ˆä¾›åº”å•†ï¼‰-- everyone involved in moving a product from supplier to customer.
 
 Starting from one root supplier, everyone on the chain buys products from one's supplier in a price P and sell or distribute them in a price that is r% higher than P.  Only the retailers will face the customers.
 It is assumed that each member in the supply chain has exactly one supplier except the root supplier, and there is no supply cycle.
 
 Now given a supply chain, you are supposed to tell the total sales from all the retailers.
 
-ÊäÈëÃèÊö:
+è¾“å…¥æè¿°:
 Each input file contains one test case.  For each case, the first line contains three positive numbers: N (<=105), the total number of the members in the supply chain (and hence their ID's are numbered from 0 to N-1, and the root supplier's ID is 0); P, the unit price given by the root supplier; and r, the percentage rate of price increment for each distributor or retailer.  Then N lines follow, each describes a distributor or retailer in the following format:
 Ki ID[1] ID[2] ... ID[Ki]
 where in the i-th line, Ki is the total number of distributors or retailers who receive products from supplier i, and is then followed by the ID's of these distributors or retailers.  Kj being 0 means that the j-th member is a retailer, then instead the total amount of the product will be given after Kj.  All the numbers in a line are separated by a space.
 
 
-Êä³öÃèÊö:
+è¾“å‡ºæè¿°:
 For each test case, print in one line the total sales we can expect from all the retailers, accurate up to 1 decimal place.  It is guaranteed that the number will not exceed 1010.
 
-ÊäÈëÀı×Ó:
+è¾“å…¥ä¾‹å­:
 10 1.80 1.00
 3 2 3 5
 1 9
@@ -31,7 +31,7 @@ For each test case, print in one line the total sales we can expect from all the
 0 4
 0 3
 
-Êä³öÀı×Ó:
+è¾“å‡ºä¾‹å­:
 42.4
 */
 
