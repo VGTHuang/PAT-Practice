4d8834d7735d620590822236c847b000560195f8
VGTHuang <36185631+VGTHuang@users.noreply.github.com>
2018-02-20 11:25:07 +0800

    Update 1005.cpp

--- 1005.cpp
+++ 1005.cpp
@@ -1,21 +1,29 @@
 //PAT 1005, 2018/2/14
 
 /*
-ÌâÄ¿ÃèÊö
-A family hierarchy is usually presented by a pedigree tree where all the nodes on the same level belong to the same generation.  Your task is to find the generation with the largest population.
+é¢˜ç›®æè¿°
+A family hierarchy is usually presented by a pedigree tree where all the nodes on the same level 
+belong to the same generation.  Your task is to find the generation with the largest population.
 
-ÊäÈëÃèÊö:
-Each input file contains one test case.  Each case starts with two positive integers N (<100) which is the total number of family members in the tree (and hence assume that all the members are numbered from 01 to N), and M (<N) which is the number of family members who have children.  Then M lines follow, each contains the information of a family member in the following format:
+è¾“å…¥æè¿°:
+Each input file contains one test case.  Each case starts with two positive integers N (<100) which 
+is the total number of family members in the tree (and hence assume that all the members are numbered 
+from 01 to N), and M (<N) which is the number of family members who have children.  Then M lines follow, 
+each contains the information of a family member in the following format:
 
 ID K ID[1] ID[2] ... ID[K]
 
-where ID is a two-digit number representing a family member, K (>0) is the number of his/her children, followed by a sequence of two-digit ID's of his/her children. For the sake of simplicity, let us fix the root ID to be 01.  All the numbers in a line are separated by a space.
+where ID is a two-digit number representing a family member, K (>0) is the number of his/her children, 
+followed by a sequence of two-digit ID's of his/her children. For the sake of simplicity, let us fix 
+the root ID to be 01.  All the numbers in a line are separated by a space.
 
 
-Êä³öÃèÊö:
-For each test case, print in one line the largest population number and the level of the corresponding generation.  It is assumed that such a generation is unique, and the root level is defined to be 1.
+è¾“å‡ºæè¿°:
+For each test case, print in one line the largest population number and the level of the 
+corresponding generation.  It is assumed that such a generation is unique, and the root level 
+is defined to be 1.
 
-ÊäÈëÀı×Ó:
+è¾“å…¥ä¾‹å­:
 23 13
 21 1 23
 01 4 03 02 04 05
@@ -31,7 +39,7 @@ For each test case, print in one line the largest population number and the leve
 09 1 17
 10 1 18
 
-Êä³öÀı×Ó:
+è¾“å‡ºä¾‹å­:
 9 4
 */
 
