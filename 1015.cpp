75d82f4bcb654f75d4b1f2f26ab8b00a89fa5066
VGTHuang <36185631+VGTHuang@users.noreply.github.com>
2018-02-26 21:50:00 +0800

    Update 1015.cpp

--- 1015.cpp
+++ 1015.cpp
@@ -1,7 +1,7 @@
 //PAT 1015, 2018/2/24
 
 /*
-ÌâÄ¿ÃèÊö
+é¢˜ç›®æè¿°
 It is said that in 2013, there were about 100 graduate schools ready to proceed over 40,000 applications in Zhejiang Province.  It would help a lot if you could write a program to automate the admission procedure.
 Each applicant will have to provide two grades: the national entrance exam grade GE, and the interview grade GI.  The final grade of an applicant is (GE + GI) / 2.  The admission rules are:
 
@@ -10,16 +10,16 @@ If there is a tied final grade, the applicants will be ranked according to their
 Each applicant may have K choices and the admission will be done according to his/her choices: if according to the rank list, it is one's turn to be admitted; and if the quota of one's most preferred shcool is not exceeded, then one will be admitted to this school, or one's other choices will be considered one by one in order. If one gets rejected by all of preferred schools, then this unfortunate applicant will be rejected.
 If there is a tied rank, and if the corresponding applicants are applying to the same school, then that school must admit all the applicants with the same rank, even if its quota will be exceeded.
 
-ÊäÈëÃèÊö:
+è¾“å…¥æè¿°:
 Each input file contains one test case.  Each case starts with a line containing three positive integers: N (<=40,000), the total number of applicants; M (<=100), the total number of graduate schools; and K (<=5), the number of choices an applicant may have.
 In the next line, separated by a space, there are M positive integers.  The i-th integer is the quota of the i-th graduate school respectively.
 Then N lines follow, each contains 2+K integers separated by a space.  The first 2 integers are the applicant's GE and GI, respectively.  The next K integers represent the preferred schools.  For the sake of simplicity, we assume that the schools are numbered from 0 to M-1, and the applicants are numbered from 0 to N-1.
 
 
-Êä³öÃèÊö:
+è¾“å‡ºæè¿°:
 For each test case you should output the admission results for all the graduate schools.  The results of each school must occupy a line, which contains the applicants' numbers that school admits.  The numbers must be in increasing order and be separated by a space.  There must be no extra space at the end of each line.  If no applicant is admitted by a school, you must output an empty line correspondingly.
 
-ÊäÈëÀı×Ó:
+è¾“å…¥ä¾‹å­:
 11 6 3
 2 1 2 2 2 3
 100 100 0 1 2
@@ -34,7 +34,7 @@ For each test case you should output the admission results for all the graduate 
 70 80 1 2 3
 100 100 0 2 4
 
-Êä³öÀı×Ó:
+è¾“å‡ºä¾‹å­:
 0 10
 3
 5 6 7
@@ -42,7 +42,7 @@ For each test case you should output the admission results for all the graduate 
 
 1 4
 
-²âÊÔÓÃÀı:
+æµ‹è¯•ç”¨ä¾‹:
 20 3 3
 4 7 8
 5 3 1 1 0
@@ -66,12 +66,14 @@ For each test case you should output the admission results for all the graduate 
 8 9 1 1 0
 4 9 0 1 2
 
-¶ÔÓ¦Êä³öÓ¦¸ÃÎª:
+å¯¹åº”è¾“å‡ºåº”è¯¥ä¸º:
 
 6 16 17 19
 0 2 8 9 15 18
 1 3 4 5 10 11 12 14
 */
+
+//messy; passed all but one test
 
 #include <iostream>
 #include <vector>
