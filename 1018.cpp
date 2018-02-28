b20ec0249f0c44bb75524f813b263d15bef964e5
VGTHuang <36185631+VGTHuang@users.noreply.github.com>
2018-02-28 19:32:34 +0800

    Update 1018.cpp

--- 1018.cpp
+++ 1018.cpp
@@ -1,20 +1,20 @@
 //PAT 1018, 2018/2/28
 
 /*
-ÌâÄ¿ÃèÊö
+é¢˜ç›®æè¿°
 If you are a fan of Harry Potter, you would know the world of magic has its own currency system -- as Hagrid explained it to Harry, "Seventeen silver Sickles to a Galleon and twenty-nine Knuts to a Sickle, it's easy enough."  Your job is to write a program to compute A+B where A and B are given in the standard form of "Galleon.Sickle.Knut" (Galleon is an integer in [0, 10^7], Sickle is an integer in [0, 17), and Knut is an integer in [0, 29)).
 
-ÊäÈëÃèÊö:
+è¾“å…¥æè¿°:
 Each input file contains one test case which occupies a line with A and B in the standard form, separated by one space.
 
 
-Êä³öÃèÊö:
+è¾“å‡ºæè¿°:
 For each test case you should output the sum of A and B in one line, with the same format as the input.
 
-ÊäÈëÀı×Ó:
+è¾“å…¥ä¾‹å­:
 3.2.1 10.16.27
 
-Êä³öÀı×Ó:
+è¾“å‡ºä¾‹å­:
 14.1.28
 */
 
@@ -25,7 +25,6 @@ For each test case you should output the sum of A and B in one line, with the sa
 using namespace std;
 
 const int sickle2knut = 29, galleon2sickle = 17;
-
 
 class hpcurrency {
 public:
